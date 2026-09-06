87e906648680cd46a8398d1209790fe0eb731bac
masuds parvaz <masudparvaz883@gmail .com>
2026-09-06 19:02:05 +0600

    change axios config

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
+import { configs } from "@eslint/js";
 import axios from "axios";
 
 // Accessing the variable from your secure Config object
-const API_URL = Config.API_URL || "http://localhost:5000";
+const API_URL = configs.API_URL || "http://localhost:5000";
 
 const api = axios.create({
     baseURL: API_URL,
