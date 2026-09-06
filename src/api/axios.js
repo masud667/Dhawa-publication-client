dbbf76030851c64e64d996c48a17eb791971d600
masuds parvaz <masudparvaz883@gmail .com>
2026-09-06 18:56:31 +0600

    change axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL =
-    import.meta.env.VITE_API_URL || "http://localhost:5000";
+// Accessing the variable from your secure Config object
+const API_URL = Config.API_URL || "http://localhost:5000";
 
 const api = axios.create({
     baseURL: API_URL,
